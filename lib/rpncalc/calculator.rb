b11b62883343ef2aaf7f5e8ff982adbe3f45f084
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-05-26 13:27:31 -0700

    push down all RPN specifics into RPNCalc

--- lib/rpncalc/calculator.rb
+++ lib/rpncalc/calculator.rb
@@ -1,22 +1,12 @@
 module Calculator
   include RPNOperation
-  include FloatToSimpleFormat 
-  attr_accessor :stack
-  attr_accessor :input
-  def calculate(input)
-    self.stack ||= []
-    self.input = input
-    return run_op
-  end
-  def run_op
+   def calculate(input)
     result = nil
     begin
-    result = operation(self.input,stack)
-    self.stack << format_input(result)
-    result = format_result(result)
+      result = operation(input)
     rescue
       result = "#{$!.message} - Reset Calculator"
-      self.stack = nil
+      reset
     end
     return  result
   end
